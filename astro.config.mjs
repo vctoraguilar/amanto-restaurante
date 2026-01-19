851a03b61d2a895b8bfb5218457d46c5c7e22f0d
vctoraguilar <vicaguilarpi@upt.pe>
2026-01-18 21:47:29 -0500

    sitemap limpio

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel/serverless';
-import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [tailwind(), react(), sitemap()],
+  integrations: [tailwind(), react()],
   site: 'https://amantorestaurante.com',
   i18n: {
     defaultLocale: 'en',

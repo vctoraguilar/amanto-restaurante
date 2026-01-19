8f5ea7d5bc6c0df13bccc58d1611544d20983fa3
vctoraguilar <vicaguilarpi@upt.pe>
2026-01-18 21:32:07 -0500

    gitignore

--- .vercel/output/_functions/chunks/TranslationScript_DavLuZwl.mjs
+++
@@ -1,9 +0,0 @@
-import { c as createComponent, r as renderTemplate } from './astro/server_BPEG-9I1.mjs';
-import 'kleur/colors';
-import 'clsx';
-
-const $$TranslationScript = createComponent(($$result, $$props, $$slots) => {
-  return renderTemplate``;
-}, "F:/Amanto/web_amanto/src/components/TranslationScript.astro", void 0);
-
-export { $$TranslationScript as $ };
